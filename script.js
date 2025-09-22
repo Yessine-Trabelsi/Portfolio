2564e8a867294627547c3fe3debbae3876acf35e
Yessine-Trabelsi <145761045+Yessine-Trabelsi@users.noreply.github.com>
2025-09-22 19:07:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+resumeBtn = document.querySelector("#resume");
+workBtn = document.querySelector("#myWork");
+skillsBtn = document.querySelector("#mySkills");
+const show = document.querySelector("#show");
+
+const resume = document.createElement("div");
+resume.innerHTML = `
+<h4>Yessine Trabelsi</h4>
+<h4>Frontend Developer | Computer Enginnering Student</h4>
+<br/>
+<p>Passionate Computer Engineering student with hands-on experience building web applications.
+ Skilled in full-stack development and UI/UX design, actively seeking opportunities to apply 
+ these skills in real-world projects.</p>
+ <div id="resumeP">
+ <h4>Education :</h4>
+<strong>Those are the courses I attended : </strong>
+<p>-Frontend development: HTML/CSS/Javascript</p>
+<p>-How to design your programms</p>
+<p>-Computer engineering in university: network 
+design,protcols/security/exploitation systems/data base</p>
+<p>-Studied in mathematics section in high school </p>
+</div>
+<br/>
+<h4>Expereince:</h4>
+<p>-This portfolio😅 It contains 4 simple web pages go check them on projects</p>
+`;
+
+const skills = document.createElement("div");
+skills.classList.add("skills");
+skills.innerHTML = `
+<p>-Frontend design ✅🔥🔥🔥</p>
+<p>-API integration ✅</p>
+<p>-Data Base integration ✅</p>
+<p>-HTML/CSS  ✅</p>
+<p>-Javascript ✅</p>
+<p>-Figma   ✅</p>
+<p>-UX/UI   ✅</p>
+<p>-Programming  ✅🔥🔥🔥</p>
+<p>-Python   ✅</p>
+<p>-Math   ✅</p>
+
+<strong>Languages: English/Frensh/Arabic</strong>
+<br/>
+
+`;
+const projects = document.createElement("div");
+projects.innerHTML = `
+🛒<a href="https://yessine-trabelsi.github.io/e-commerce/" target="_blank" style="text-decoration: underline;">E-Commerce Web Site!</a>
+<br/>
+🎬️<a href="https://yessine-trabelsi.github.io/Movie-Search/" target="_blank" style="text-decoration: underline;">Movie Search</a>
+<br/>
+🧾<a href="https://yessine-trabelsi.github.io/To-do-app/" target="_blank" style="text-decoration: underline;">To Do App</a>
+<br/>
+🎮️<a href="https://yessine-trabelsi.github.io/tic_tac_toe/" target="_blank" style="text-decoration: underline;">Tic Tac Toe</a>
+<br/>
+<br/>
+`;
+
+skillsBtn.addEventListener("click", () => {
+  show.innerHTML = ``;
+
+  show.appendChild(skills);
+});
+
+workBtn.addEventListener("click", () => {
+  show.innerHTML = ``;
+  show.appendChild(projects);
+});
+
+resumeBtn.addEventListener("click", () => {
+  show.innerHTML = ``;
+  show.appendChild(resume);
+});
